c6c9b9e653fb6ff9340d29fec9dae3dd9225b339
olehpetrovasyliev <olegvasiilyevv@gmail.com>
2023-05-03 14:59:06 +0300

    task2

--- index.js
+++ index.js
@@ -1,8 +1,17 @@
 //TODO:===================================
     //Порахувати середню зарплатню співробітників і округлити число до сотих
-     const Israel = '1600$'
-     const Alexander = '2350$'
-     const Dustin = '1344$'
+//      const Israel = '1600$'
+//      const Alexander = '2350$'
+//      const Dustin = '1344$'
 
-     const sum = ((Number.parseInt(Israel) + Number.parseInt(Alexander) + Number.parseInt(Dustin)) / 3).toFixed(2)
-     console.log(sum)
+//      const sum = ((Number.parseInt(Israel) + Number.parseInt(Alexander) + Number.parseInt(Dustin)) / 3).toFixed(2)
+// console.log(sum)
+     
+//TODO:===================================
+    // Запитати у користувача "скільки апельсинів він хоче купити?",а потім вивести alert 
+    //"Користувач хоче купити <число> апельсинів!"
+    
+// console.log(1);
+// const fruitsNumber = prompt("скільки апельсинів він хоче купити?");
+// console.log(2);
+// alert(`Користувач хоче купити ${fruitsNumber} апельсинів!`)
