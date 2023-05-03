c679d9983602d87ac19ac636f26129145ad3eb74
Kushnir2205 <kushnirroman22@gmail.com>
2023-05-03 14:44:51 +0300

    module1

---
+++ index.js
@@ -0,0 +1,8 @@
+//TODO:===================================
+    //Порахувати середню зарплатню співробітників і округлити число до сотих
+     const Israel = '1600$'
+     const Alexander = '2350$'
+     const Dustin = '1344$'
+
+     const sum = ((Number.parseInt(Israel) + Number.parseInt(Alexander) + Number.parseInt(Dustin)) / 3).toFixed(2)
+     console.log(sum)
