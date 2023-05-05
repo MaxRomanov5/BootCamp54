762bef28d1c30bbc7822ee3091fa0df795191030
IsaienkoViktor <isaenkov1986@gmail.com>
2023-05-05 14:27:39 +0300

    Create index.js

---
+++ index.js
@@ -0,0 +1,15 @@
+//TODO:===================================
+//Порахувати витрати компанії за квартал
+  
+
+// const firstq = [1500,1200,2000]
+// const secondq = [100,1700,2500]
+// const thirdq = [400, 900, 700]
+// let sumTotal = 0;
+// const sumArray = firstq.concat(secondq).concat(thirdq);
+
+// for (const number of sumArray) {
+//     sumTotal += number;
+// }
+// console.log(sumTotal);
+// console.log(sumArray);
