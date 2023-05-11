3c9214484e6e40b3104f1b4018febe6f255354bc
Violete86 <115045476+Violete86@users.noreply.github.com>
2023-05-11 14:09:01 +0200

    task 2

--- index.js
+++ index.js
@@ -6,24 +6,49 @@
 // та поветрає рядок 'Користувач винен банку <число>!'
 //
 
-function bankAdmin(functionDebtor, functionNotDebtor, array) {
-  let sum = 0;
-  array.forEach((value) => (sum += value));
-  if (sum >= 0) {
-   return functionNotDebtor(sum);
-  } else {
-    return functionDebtor(sum);
-  }
-}
+// function bankAdmin(functionDebtor, functionNotDebtor, array) {
+//   let sum = 0;
+//   array.forEach((value) => (sum += value));
+//   if (sum >= 0) {
+//    return functionNotDebtor(sum);
+//   } else {
+//     return functionDebtor(sum);
+//   }
+// }
 
-function userDebtor(negativeSum) {
-  return `Користувач винен банку ${negativeSum}!`;
-}
+// function userDebtor(negativeSum) {
+//   return `Користувач винен банку ${negativeSum}!`;
+// }
 
-function userNotDebtor(positiveSum) {
-  return `Користувач не боржник, сума на його балансі ${positiveSum}!`;
-}
+// function userNotDebtor(positiveSum) {
+//   return `Користувач не боржник, сума на його балансі ${positiveSum}!`;
+// }
 
-console.log(bankAdmin(userDebtor, userNotDebtor, [100, 500, -700]));
-console.log(bankAdmin([100, 500, -600]));
+// console.log(bankAdmin(userDebtor, userNotDebtor, [100, 500, -700]));
+// console.log(bankAdmin([100, 500, -600]));
 // console.log(bankAdmin([100, 500, -300, 600]));
+
+
+
+//TODO:============================
+//Написати стрілочну функцію,яка буде рахувати суму чисел незалежно від кількості аргументів forEach
+// і приймає колбек який виводить рядок `Загальна сумма >num>!`
+
+
+const sumNumbers = (messageFunc, ...numbers) => {
+
+  let totalSum = 0;
+
+  numbers.forEach(number => {
+    totalSum += number;
+  })
+  return messageFunc(totalSum);
+
+   
+}
+
+const message = (num) => `Загальна сумма ${num}`;
+
+console.log(sumNumbers(message, 1,1,2,2,3,4,5,6));
+
+console.log(sumNumbers(message, 15,17,18,2,92,39,4,50,6));
