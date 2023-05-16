3bed997e7d5f0e3239c75f7dd52c782a1d2f9871
Valeriy94-ua <128257210+Valeriy94-ua@users.noreply.github.com>
2023-05-16 14:34:49 +0300

    task 1

---
+++ index.js
@@ -0,0 +1,12 @@
+//Написати функцію додавання,віднімання грошей до балансу та підвязати її до аккаунту
+//Написати функцію обнулення рахунку
+
+// const account = {
+//   balance: 0,
+// };
+// function addMoney(amount) {
+//   console.log(this);
+//   this.balance += amount;
+// }
+// addMoney.call(account, 300);
+// console.log(account);
