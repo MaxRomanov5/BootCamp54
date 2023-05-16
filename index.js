3681e8a6b467dc54fbaf5b934f96d7ef74d21cf2
IsaienkoViktor <isaenkov1986@gmail.com>
2023-05-16 15:17:30 +0300

    Update index.js

--- index.js
+++ index.js
@@ -19,3 +19,42 @@
 // bindAddMoney(500);
 // console.log(account);
 // addMoney(300);
+
+
+///Додати classTutor2 всі методи classTutor1
+
+const classTutor1 = {
+
+    students: [{name:'kriss',grades:{math: 9,geography: 10,physics:7}},
+    {name:'jane',grades:{math: 6,geography: 10,physics:7}},
+    {name:'alex',grades:{math: 9,geography: 4,physics:7}},
+    {name:'gregory',grades:{math: 12,geography: 12,physics:12}},
+    {name:'steve',grades:{math: 11,geography: 8,physics:6}},
+    {name:'julia',grades:{math: 4,geography: 9,physics:6}},],
+
+
+    getStudents(){
+        return this.students
+    },
+    addStudent(name,math,geography,physics){
+      this.students.push({name,grades:{math,geography,physics}}) 
+          }
+        }
+
+        const classTutor2 = {
+
+          students: [{name:'kriss',grades:{math: 9,geography: 10,physics:7}},
+                { name: 'jane', grades: { math: 6, geography: 10, physics: 7 } }],
+            
+            getStudents: classTutor1.getStudents.bind(classTutor1),
+            addStudent: classTutor1.addStudent
+            
+}
+        
+        // console.log(classTutor1.getStudents);
+        // console.log(classTutor2.addStudent("Max", 12, 7, 10));
+        // console.log(classTutor2.getStudents());
+        console.log(classTutor2.getStudents());
+
+        
+        
