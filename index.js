c32840f151b822e8fb878953355d224c0aacd6f8
endorf8n <128092594+endorf8n@users.noreply.github.com>
2023-05-18 14:22:56 +0300

    m6 task-1

---
+++ index.js
@@ -0,0 +1,17 @@
+//Звернутися до елемента за допомогою класу,id та атрибуту
+// Змінити йому textContent на 'Ми змінили текст'
+//Змінити йому клас 'textClass' на 'text'
+// Затоглити клас 'open'
+
+// const elementEl1 = document.querySelector(".textClass");
+// const elementEl2 = document.querySelector("#textId");
+// const elementEl3 = document.querySelector("[data-info='textData']");
+
+// console.log(elementEl1);
+// console.log(elementEl2);
+// console.log(elementEl3);
+
+// elementEl1.textContent = "Ми змінили йому текст";
+// elementEl1.classList.replace("textClass", "text");
+// elementEl1.classList.toggle("open");
+// elementEl1.classList.toggle("open");
