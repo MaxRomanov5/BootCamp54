c02711b79c29b7e1f250aaeab310808598b459da
olehpetrovasyliev <olegvasiilyevv@gmail.com>
2023-05-24 14:47:36 +0300

    Update index.js

--- index.js
+++ index.js
@@ -8,21 +8,31 @@
 // }
 // buttonEl.addEventListener("click", _.throttle(onButtonClick, 2000));
 
-
 //-------------------
 //написати функцію,яка буде рахувати,скільки ми можемо безперервно(не більше 0.5секунд) натиснути на кнопку разів
-const buttonEl = document.querySelector('[type="button"]');
+// const buttonEl = document.querySelector('[type="button"]');
 
-let counter = 0;
+// let counter = 0;
 
-buttonEl.addEventListener("click", (event) => {
-    counter += 1;
-    
-});
+// buttonEl.addEventListener("click", (event) => {
+//     counter += 1;
 
-buttonEl.addEventListener("click", _.debounce(quantityClick, 500));
+// });
 
-function quantityClick() {
-    console.log(`Ви клікнули ${counter} разів`);
-    counter = 0;
-}
+// buttonEl.addEventListener("click", _.debounce(quantityClick, 500));
+
+// function quantityClick() {
+//     console.log(`Ви клікнули ${counter} разів`);
+//     counter = 0;
+// }
+
+// const recursion = () => {
+//   console.log(1);
+//   _.debounce(recursion(), 500);
+// };
+
+// const newRecursion = _.debounce(() => {
+//   console.log(1);
+//   newRecursion();
+// }, 5000);
+// newRecursion();
