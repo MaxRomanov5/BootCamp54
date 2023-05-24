80cd3656b4afdb42c5893428559df8fca11c1459
YuriyMikh <pamukkale1717@gmail.com>
2023-05-24 14:33:58 +0300

    task-2

--- index.js
+++ index.js
@@ -7,3 +7,22 @@
 //   console.log("Shoot");
 // }
 // buttonEl.addEventListener("click", _.throttle(onButtonClick, 2000));
+
+
+//-------------------
+//написати функцію,яка буде рахувати,скільки ми можемо безперервно(не більше 0.5секунд) натиснути на кнопку разів
+const buttonEl = document.querySelector('[type="button"]');
+
+let counter = 0;
+
+buttonEl.addEventListener("click", (event) => {
+    counter += 1;
+    
+});
+
+buttonEl.addEventListener("click", _.debounce(quantityClick, 500));
+
+function quantityClick() {
+    console.log(`Ви клікнули ${counter} разів`);
+    counter = 0;
+}
