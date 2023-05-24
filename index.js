fdfd9baab22b5e2fed537d20d670ad5ea85a65e0
endorf8n <128092594+endorf8n@users.noreply.github.com>
2023-05-24 14:17:37 +0300

    task-1

---
+++ index.js
@@ -0,0 +1,9 @@
+//написати функцію "дробовик"
+//ми здійснюємо по кліку на кнопку постріл,але не частіше ніж у 2 секунди,бо потрібна перезарядка
+
+// const buttonEl = document.querySelector('[type="button"]');
+
+// function onButtonClick() {
+//   console.log("Shoot");
+// }
+// buttonEl.addEventListener("click", _.throttle(onButtonClick, 2000));
