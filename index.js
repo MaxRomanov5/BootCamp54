c3e4ab9346073567c142eedc385fdb1444d26baa
YuriyMikh <pamukkale1717@gmail.com>
2023-05-30 14:31:35 +0300

    task1

---
+++ index.js
@@ -0,0 +1,27 @@
+//Запустити інтервал ,який щосекундно буде логувати поточний рахунок total
+// Кнопка increment повинна кожну секунду збільшувати значення на 1,яке додається до total
+// Кнопка decrement має кожну секунду зменшувати значення на 1,яке додається до total
+//якщо total менше ніж -50 або більше ніж 50 інтервал видаляється
+
+// const incrementButtonRef = document.querySelector(".increment");
+// const decrementButtonRef = document.querySelector(".decrement");
+
+// let total = 0;
+// let count = 0;
+
+// const intID = setInterval(() => {
+//   total += count;
+//   console.log(total);
+//     if (total > 50 || total < -50) {
+//       console.log('end');
+//     clearInterval(intID);
+//   }
+// }, 1000);
+
+// incrementButtonRef.addEventListener("click", () => {
+//   count += 1;
+// });
+
+// decrementButtonRef.addEventListener("click", () => {
+//   count -= 1;
+// });
