efaee51f21c10329f6b6aa58b1fcfde73e954291
olehpetrovasyliev <olegvasiilyevv@gmail.com>
2023-06-01 20:37:54 +0300

    Update fetchPockemon.js

--- fetchPockemon.js
+++ fetchPockemon.js
@@ -1,16 +1,14 @@
+// https://api.thecatapi.com/v1/images/search
 
-export default function fetchPockemon (namePock){
-
-    return fetch (`https://pokeapi.co/api/v2/pokemon/${namePock}`).
-    then (response => {
-        if (!response.ok) {
-            throw new Error (response.status);
-        }
-        return response.json();
+export const fetchBreeds = (breed) => {
+  return fetch(`https://api.thecatapi.com/v1/images/${breed}`)
+    .then((response) => {
+      if (!response.ok) {
+        throw new Error(response.status);
+      }
+      return response.json();
     })
-    .catch(error => {
-        console.log(error)
-    })
-}
-
-
+    .catch((error) => {
+      console.log(error);
+    });
+};
