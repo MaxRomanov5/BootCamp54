467060c5f5bda7807f63f851f42d36f6109166a3
endorf8n <128092594+endorf8n@users.noreply.github.com>
2023-06-01 15:09:08 +0300

    task-2

--- index.js
+++ index.js
@@ -1,3 +1,20 @@
-import getPockemon from './fetchPockemon.js';
+import getPockemon from "./fetchPockemon.js";
 
-getPockemon ('pikachu').then (data=>console.log(data))
+const refs = {
+  input: document.querySelector('[type="text"]'),
+  button: document.querySelector("button"),
+  container: document.querySelector(".container"),
+};
+
+refs.button.addEventListener("click", onBtnClick);
+
+function onBtnClick() {
+  const inputValue = refs.input.value.toLowerCase().trim();
+  getPockemon(inputValue).then((data) => {
+    console.log(data);
+    const markup = `<div><p>${data.name}</p><img src="${data.sprites.other.dream_world.front_default}" alt="${data.name}"></div>`;
+    refs.container.innerHTML = markup;
+  });
+}
+
+getPockemon("pikachu").then((data) => console.log(data));
