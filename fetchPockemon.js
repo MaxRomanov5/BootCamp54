151e701e6b90a6f222dfa34de46451d47292f903
Valeriy94-ua <128257210+Valeriy94-ua@users.noreply.github.com>
2023-06-02 14:38:36 +0300

    task-1

--- fetchPockemon.js
+++ fetchPockemon.js
@@ -1,12 +1,25 @@
-export const getPockemon = (namePock) {
+export function getPockemon(namePock) {
   return fetch(`https://pokeapi.co/api/v2/pokemon/${namePock}`)
-    .then(response => {
+    .then((response) => {
       if (!response.ok) {
         throw new Error(response.status);
       }
       return response.json();
     })
-    .catch(error => {
+    .catch((error) => {
+      console.log(error);
+    });
+}
+
+export function getPockemonList(namePock) {
+  return fetch(`https://pokeapi.co/api/v2/pokemon?offset=0&limit=5`)
+    .then((response) => {
+      if (!response.ok) {
+        throw new Error(response.status);
+      }
+      return response.json();
+    })
+    .catch((error) => {
       console.log(error);
     });
 }
--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-import getPockemon from "./fetchPockemon.js";
+import { getPockemon, getPockemonList } from "./fetchPockemon.js";
 
 const refs = {
   input: document.querySelector('[type="text"]'),
@@ -17,4 +17,13 @@ function onBtnClick() {
   });
 }
 
-getPockemon("pikachu").then((data) => console.log(data));
+getPockemonList().then((data) => {
+  console.log(data);
+  data.results.forEach((dataItem) => {
+    getPockemon(dataItem.name).then((data) => {
+      console.log(data);
+      const markup = `<div><p>${data.name}</p><img src="${data.sprites.other.dream_world.front_default}" alt="${data.name}"></div>`;
+      refs.container.insertAdjacentHTML("beforeend", markup);
+    });
+  });
+});
