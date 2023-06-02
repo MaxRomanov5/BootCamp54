8e40c8d467081e05441b44087917fe8bed985f25
Anna-Voronina <anna.vs2512@gmail.com>
2023-06-02 20:02:39 +0300

    task-3

--- index.js
+++ index.js
@@ -2,12 +2,14 @@ import { getPockemon, getPockemonList } from "./fetchPockemon.js";
 
 const refs = {
   input: document.querySelector('[type="text"]'),
-  button: document.querySelector("button"),
+  prevButton: document.querySelector(".prev"),
+  nextButton: document.querySelector(".next"),
   container: document.querySelector(".container"),
-  loader: document.querySelector('.loader'),
+  loader: document.querySelector(".loader"),
 };
 
 let offset = 0;
+let totalPokemons = 0;
 
 // refs.button.addEventListener("click", onBtnClick);
 
@@ -21,33 +23,58 @@ let offset = 0;
 // }
 
 function loadPockemons() {
-  refs.loader.classList.toggle('invisible');
-  refs.container.classList.toggle('invisible');
+  refs.loader.classList.toggle("invisible");
+  refs.container.classList.toggle("invisible");
 
-  getPockemonList(offset).then((data) => {
-    console.log(data);
-    data.results.forEach((dataItem) => {
-      getPockemon(dataItem.name).then((data) => {
-        console.log(data);
-        const markup = `<div><p>${data.name}</p><img src="${data.sprites.other.dream_world.front_default}" alt="${data.name}"></div>`;
-        refs.container.insertAdjacentHTML("beforeend", markup);
+  getPockemonList(offset)
+    .then((data) => {
+      console.log(data);
+      totalPokemons = data.count;
+      data.results.forEach((dataItem) => {
+        getPockemon(dataItem.name).then((data) => {
+          console.log(data);
+          const markup = `<div class="container-item"><p class="item-name">${data.name}</p><img src="${data.sprites.front_default}" alt="${data.name}"></div>`;
+          refs.container.insertAdjacentHTML("beforeend", markup);
+        });
       });
+    })
+    .finally(() => {
+      setTimeout(() => {
+        refs.loader.classList.toggle("invisible");
+        refs.container.classList.toggle("invisible");
+      }, 500);
     });
-  })
-  .finally(() => {
-    setTimeout(() => {
-      refs.loader.classList.toggle('invisible');
-      refs.container.classList.toggle('invisible');
-      }, 500)
-  });
 }
 loadPockemons();
 
-const getNewPockemon =()=>{
-    refs.container.innerHTML = '';
+const getNewPockemon = () => {
+  refs.container.innerHTML = "";
+
+  if (offset >= totalPokemons - 5) {
+    offset = 0;
+  } else {
     offset += 5;
+  }
+
+  console.log(offset);
   loadPockemons();
- }
+};
 
+const onPrevBtnClick = () => {
+  // if (offset === 0) {
+  //   Notiflix.Notify.failure("Ви знаходитесь на першій сторінці!");
+  //   return;
+  // }
 
-refs.button.addEventListener('click',getNewPockemon)
+  if (offset <= 0) {
+    offset = totalPokemons - 5;
+  } else {
+    offset -= 5;
+  }
+
+  refs.container.innerHTML = "";
+  loadPockemons();
+};
+
+refs.prevButton.addEventListener("click", onPrevBtnClick);
+refs.nextButton.addEventListener("click", getNewPockemon);
