d33f75bd79d649e4af22786acf4eb831d905d9a9
Vyachuk <Slavkoka045@gmail.com>
2023-06-02 15:10:37 +0300

    add pagination for pokemon

--- index.js
+++ index.js
@@ -4,6 +4,7 @@ const refs = {
   input: document.querySelector('[type="text"]'),
   button: document.querySelector("button"),
   container: document.querySelector(".container"),
+  loader: document.querySelector('.loader'),
 };
 
 let offset = 0;
@@ -19,29 +20,33 @@ let offset = 0;
 //   });
 // }
 
-getPockemonList(offset).then((data) => {
-  console.log(data);
-  data.results.forEach((dataItem) => {
-    getPockemon(dataItem.name).then((data) => {
-      console.log(data);
-      const markup = `<div><p>${data.name}</p><img src="${data.sprites.other.dream_world.front_default}" alt="${data.name}"></div>`;
-      refs.container.insertAdjacentHTML("beforeend", markup);
+function loadPockemons() {
+  refs.loader.classList.toggle('invisible');
+  refs.container.classList.toggle('invisible');
+
+  getPockemonList(offset).then((data) => {
+    console.log(data);
+    data.results.forEach((dataItem) => {
+      getPockemon(dataItem.name).then((data) => {
+        console.log(data);
+        const markup = `<div><p>${data.name}</p><img src="${data.sprites.other.dream_world.front_default}" alt="${data.name}"></div>`;
+        refs.container.insertAdjacentHTML("beforeend", markup);
+      });
     });
+  })
+  .finally(() => {
+    setTimeout(() => {
+      refs.loader.classList.toggle('invisible');
+      refs.container.classList.toggle('invisible');
+      }, 500)
   });
-});
+}
+loadPockemons();
+
 const getNewPockemon =()=>{
     refs.container.innerHTML = '';
     offset += 5;
-    getPockemonList(offset).then((data) => {
-  console.log(data);
-  data.results.forEach((dataItem) => {
-    getPockemon(dataItem.name).then((data) => {
-      console.log(data);
-      const markup = `<div><p>${data.name}</p><img src="${data.sprites.other.dream_world.front_default}" alt="${data.name}"></div>`;
-      refs.container.insertAdjacentHTML("beforeend", markup);
-    });
-  });
-});
+  loadPockemons();
  }
 
 
